6a794530331534bad43e8f833db0bea22263c0c0
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-05-25 05:46:36 +0300

    It will make the calculator Funtional

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,26 @@
-function addDigit(digit) {
-    var resultField = document.getElementById("values");
-    resultField.innerHTML += digit;
-    return false;
-    }
+let input=document.getElementById("input");
+let buttons= Array.from(document.getElementsByClassName("button"));
+buttons.map(button =>{
+    button.addEventListener('click', (e)=>{
+        switch(e.target.innerText){
+            case'C':
+            input.innerText='';
+            break;
+     
+            case '=':
+                try{
+                    input.innerText=eval(input.innerText);
+
+                }
+                catch{
+                    input.innerText='Error';
+                }
+                break; 
+                
+                default:
+                    input.innerText += e.target.innerText;   
+        }
+    })
+})
+
+
